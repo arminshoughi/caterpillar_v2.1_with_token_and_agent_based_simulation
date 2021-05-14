5d4836d83cd5be0de07193bab08a0ac685fc6543
armin shoughi <arminshoughi@gmail.com>
2021-05-14 23:17:18 +0430

    working on multi-instance simulation

--- prototype/execution-panel/src/app/modeler/modeler.component.ts
+++ prototype/execution-panel/src/app/modeler/modeler.component.ts
@@ -12,7 +12,6 @@ import { CustomPropertiesProvider } from "./props-provider";
 
 const fs = require("fs");
 const async = require("async");
-// const compiler = require("bpmn-sol");
 
 const customPaletteModule = {
   paletteProvider: ["type", PaletteProvider]
@@ -104,33 +103,66 @@ export class ModelerComponent implements OnInit {
       this.router.navigateByUrl('/dashboard');
   }
   agentBaseSimulation(){
-    
     this.modeler.saveXML({ format: true }, (err: any, xml: string) => {
-      for (let i = 0; i < this.modeler.definitions.rootElements.length; i++) 
-      {
-        if (this.modeler.definitions.rootElements[i].$type === 'bpmn:Process') 
-        {
-          if (this.processStorage.hasModel(this.modeler.definitions.rootElements[i].id)) 
-          {
-            this.modelText =
-              'The selected ID exists on the Workspace. Please, change this value and try again.';
-          } 
-          else if (!this.modeler.definitions.rootElements[i].name || this.modeler.definitions.rootElements[i].name === '') {
-            this.modelText =
-              'The Name of the model cannot be empty. Please, update this value and try again.';
-          } 
-          else {
-            // this.goToDashborad();
-            this.processStorage.modelId = this.modeler.definitions.rootElements[i].id;
-            this.processStorage.getRegisterModel(xml);
-            this.modelText =
-              'Working in Model Registration. Please, take into account that this may require some seconds.';
-          }
-          break;
+      // console.log(xml)
+      // var file = require('file-saver')
+      // var blob = new Blob([xml], {type: "text/plain;charset=utf-8"});
+      // alert("For simulate bpmn and see the result, first when download dialog is shown download bpmn file, then you will be redirected to simulation tab and when simulatio tab was loaded successfuly, upload this file and click on 'Continue' button to starting simulate BPMN.")
+      // file.saveAs(blob, "test.bpmn"); 
+      
+      localStorage.setItem('xml',xml.split('\n').join('\\'));      
+      window.open("http://127.0.0.1:8080",'_blank');
+    })
+
+    function setCookie(cname, cvalue) {
+      var d = new Date();
+      d.setTime(d.getTime() + (10000));
+      var expires = "expires="+ d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/; flavor=choco; SameSite=Lax";
+    }
+
+    function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
         }
       }
+      return "";
+    }
+    
+    // this.modeler.saveXML({ format: true }, (err: any, xml: string) => {
+    //   for (let i = 0; i < this.modeler.definitions.rootElements.length; i++) 
+    //   {
+    //     if (this.modeler.definitions.rootElements[i].$type === 'bpmn:Process') 
+    //     {
+    //       if (this.processStorage.hasModel(this.modeler.definitions.rootElements[i].id)) 
+    //       {
+    //         this.modelText =
+    //           'The selected ID exists on the Workspace. Please, change this value and try again.';
+    //       } 
+    //       else if (!this.modeler.definitions.rootElements[i].name || this.modeler.definitions.rootElements[i].name === '') {
+    //         this.modelText =
+    //           'The Name of the model cannot be empty. Please, update this value and try again.';
+    //       } 
+    //       else {
+    //         // this.goToDashborad();
+    //         this.processStorage.modelId = this.modeler.definitions.rootElements[i].id;
+    //         this.processStorage.getRegisterModel(xml);
+    //         this.modelText =
+    //           'Working in Model Registration. Please, take into account that this may require some seconds.';
+    //       }
+    //       break;
+    //     }
+    //   }
       
-    })
+    // })
     
   }
 }
